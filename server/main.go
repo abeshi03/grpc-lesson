29b273f1462b67f69aa22d6a9094e6cc924b78e4
abeshi03 <k_913411@yahoo.co.jp>
2022-08-24 09:34:58 +0900

    fix

--- server/main.go
+++ server/main.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"google.golang.org/grpc"
 	"grpc-lesson/pb"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
+	"time"
 )
 
 type server struct {
@@ -35,6 +38,38 @@ func (*server) ListFiles(ctx context.Context, req *pb.ListFilesRequest) (*pb.Lis
 		Filenames: filenames,
 	}
 	return res, nil
+}
+
+func (*server) Download(req *pb.DownloadRequest, stream pb.FileService_DownloadServer) error {
+	fmt.Println("Download was")
+
+	filename := req.GetFilename()
+	path := "/Users/abekouhei/Documents/abeshi-projects/grpc-lesson/storage" + filename
+
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, 5)
+
+	for {
+		n, err := file.Read(buf)
+		if n == 0 || err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		res := &pb.DownloadResponse{Data: buf[:n]}
+		sendErr := stream.Send(res)
+		if sendErr != nil {
+			return sendErr
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+	return nil
 }
 
 func main() {
